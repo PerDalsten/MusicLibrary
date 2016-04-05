0fe900ca05b94b0931322a51b32d28dd41bc036d
Per Dalsten Sorensen <pdalsten@gmail.com>
2016-04-04 21:23:41 -0400

    Added filter to force utf-8 for form data

---
+++ src/main/java/dk/purplegreen/musiclibrary/ui/CharacterEncodingFilter.java
@@ -0,0 +1,33 @@
+package dk.purplegreen.musiclibrary.ui;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/albums/*")
+public class CharacterEncodingFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		//Force utf-8 if not set by browser (JSF form data will be utf-8)
+		if (request.getCharacterEncoding() == null)
+			request.setCharacterEncoding("utf-8");
+
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+	}
+}
